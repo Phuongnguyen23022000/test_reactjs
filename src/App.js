38ff8de543394e3883d0dba2f017ab098be73bab
Nguuyenvanphuong <Nguyenvanphuong.23032000@gmail.com>
2023-03-10 13:18:07 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,158 @@
-import logo from './logo.svg';
+import loginService from './services/login';
+import { useEffect, useState } from 'react';
 import './App.css';
+// import Note from './components/Note';
+import axios from 'axios';
+import noteService from './services/notes';
+import Notification from './components/Notification';
+import Note from './components/Note';
 
-function App() {
+
+const App = (props) => {
+  // const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('some error happened')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null)
+
+  /////////////////////////////
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log('click', event.target)
+    const notesObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
+    noteService
+      .create(notesObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+    //method post object vao db.json 
+    // axios
+    //   .post('http://localhost:3001/notes', notesObject)
+    //   .then(response => {
+    //     setNotes(notes.concat(response.data))
+    //     setNewNote('')
+    //   })
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value);
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true);
+  // get db ve
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content} was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      window.localStorage.setItem('User', JSON.stringify(user))
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage("Wrong credentials")
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+      </div>
+      <div>
+        password
+        <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type='submit'>
+        Save
+      </button>
+    </form>
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} /> 
+      {/* {user === null ? loginForm() : noteForm()} */}
+
+      {!user && loginForm()}
+      {user && <div>
+          <p>{user.name} login in</p>
+          {noteForm()}
+        </div>
+      }
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
+      </div>
+      <ul>
+        {notesToShow.map(note => <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />)}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type='submit'>
+          Save
+        </button>
+      </form>
+
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
 
+ axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  }
+]
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App notes={notes}/>
   </React.StrictMode>
 );
 
